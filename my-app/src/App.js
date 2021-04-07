b0b9e3244008f19c0b04e7274ba39e200f6013e3
brandonTsai <brandonya125@gmail.com>
2021-04-07 07:29:53 -0700

    NanoID & DayJs for ID & Time, update favicon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,105 +1,74 @@
-import React, {useState, useEffect} from 'react'
-
-let itemID
-let itemList
-let timeIsValid = false
+import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
+import dayjs from 'dayjs'
 
 function App() {
+  const [itemList, setItemList] = useState(JSON.parse(localStorage.getItem('itemList')))
   const [time, setTime] = useState('')
   const [description, setDescription] = useState('')
-  console.log('App', itemID)
+  
+  // to avoid error to push data to itemList if it's null when no any data in localStorage
+  if(itemList === null) {
+    setItemList([])
+  }
 
   const createItem = () => {
-    console.log('Below is from createItem')
-    console.log('itemID BEFORE ++', itemID)
-    if (!timeIsValid) {
-      console.log('Please enter a valid time!')
-      return
-    }
     if (description === '') {
-      console.log('Please enter your decription...')
+      console.log('Please Enter Your Decription...')
       return
     }
-    const dataTime = time
-    const dataDescription = description
-    console.log(itemID)
-    const dataID = itemID
+    const inputID = nanoid(11)
+    const inputTime = time
+    const inputDescription = description
     const formatData = {
-        dataID,
-        dataTime,
-        dataDescription
+      inputID,
+      inputTime,
+      inputDescription
     };
-    itemID++
-    console.log('itemID AFTER ++', itemID)
-    console.log('From creataItem', itemID)
     itemList.push(formatData)
     localStorage.setItem('itemList', JSON.stringify(itemList))
+    // setItemList(itemList)
   }
 
   const readItem = () => {
-    itemList.push(JSON.parse(localStorage.getItem('itemList')))
+    // itemList.push(JSON.parse(localStorage.getItem('itemList')))
     // document.getElementById('item-list').innerHTML = register
+
     console.log(itemList)
   }
 
-  const updateItem = () => {
-    console.log("Update Connected!!")
-  }
-
-  const deleteItem = () => {
-    console.log("Delete Connected!!")
-  }
-
-  const timeValidation = (inputTime) => {
-    const timeRegex = /^\d{2}\/\d{2}\/\d{4}$/ ;
-    return timeRegex.test(inputTime)
-  }
-
-  useEffect(() => {
-    if (time === '') {
-      return
-    }
-    timeIsValid = timeValidation(time)
-    console.log(timeIsValid)
-  }, [time])
-
-  useEffect(() => {
-    itemList = JSON.parse(localStorage.getItem('itemList'))
-    if (itemList === null) {
-      itemList = []
-    }
-    console.log(itemID, itemList)
-  })
-
   return (
     <div>
-      <h1>Start Here!</h1>
-        <form>
-          <input type='text' name='time' id='time' placeholder='MM/DD/YYYY' onChange={(e) => {setTime(e.target.value)}}/>
-          <input type='text' name='description' id='description' placeholder='Description' onChange={(e) => {setDescription(e.target.value)}}/>
-        </form>
-        <div className='button'>
-          <button onClick={createItem}>Create Item</button>
-          <button onClick={readItem}>Read Item</button>
-          <button onClick={updateItem}>Update Item</button>
-          <button onClick={deleteItem}>Delete Item</button>
-        </div>
-        <div className='table-wrapper'>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th>Time</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody id='item-list'>
-              <tr>
-                 <td>Time</td>
-                 <td>Description</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <h1>Follow Your Plan!!</h1>
+      <form>
+        <input type="datetime-local" id="time"
+        name="time"
+        min="1900-01-01T00:00"
+        max="2200-12-31T00:00"
+        onChange={(e) => setTime(dayjs(e.target.value).toISOString())}
+        />
+        <input type='text' id='description'
+        name='description'
+        placeholder='Description'
+        onChange={(e) => setDescription(e.target.value)}
+        />
+      </form>
+      <div className='button'>
+        <button onClick={createItem}>Create Item</button>
+        <button onClick={readItem}>Read Item</button>
+      </div>
+      <div className='table-wrapper'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody id='item-list'>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
